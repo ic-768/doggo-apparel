d7c9f1cc0520d3a82d6fe17ffdd4715d10877987
ic768 <ic768@protonmail.com>
2024-10-03 20:08:18 +0300

    extract testimonials

---
+++ components/landing-page/testimonials/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import selfie1 from "@/images/user-selfies/selfie-1.jpg";
+import selfie2 from "@/images/user-selfies/selfie-2.jpg";
+import selfie3 from "@/images/user-selfies/selfie-3.webp";
+import { fadeIntoView } from "@/lib/motion";
+
+import TestimonialCard from "./testimonial-card";
+
+export default function Testimonials() {
+  return (
+    <section className="py-20">
+      <div className="flex flex-col gap-12 px-4 mx-auto">
+        <motion.h2
+          {...fadeIntoView}
+          className="text-4xl font-bold text-center text-gray-800"
+        >
+          What Our Customers Say
+        </motion.h2>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <TestimonialCard
+            quote="I bought this outfit for my dog's first Diwali
+                celebration, and it was a hit! The fabric is soft and
+                breathable, ensuring he was comfortable all day long. Super
+                vibrant colors and beautifully intricate designs!"
+            image={selfie1}
+            name="John Johnson"
+          />
+          <TestimonialCard
+            quote="This outfit exceeded my expectations! It’s not only
+                stylish but also very easy to put on and take off. My dog looked
+                so adorable! I'll definitely be buying more!"
+            image={selfie2}
+            name="Nicholas Nixon"
+          />
+          <TestimonialCard
+            quote="These sunglasses are a game-changer for our beach trips!
+                They fit my dog perfectly and stay in place, even during our
+                most adventurous outings. Not only do they protect his eyes from
+                the sun, but they also make him look incredibly cool! I've
+                received countless compliments, and my pup loves them."
+            image={selfie3}
+            name="Ronnie Rogers"
+          />
+          <TestimonialCard
+            quote="I can’t say enough good things about this dog cap! It’s
+                not only adorable, but it also keeps my pup cool and protected
+                from the sun during our walks. The adjustable strap makes it a
+                perfect fit, and the breathable material ensures he stays
+                comfortable."
+            image={selfie1}
+            name="Daniel Davis"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/landing-page/testimonials/testimonial-card.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image, { StaticImageData } from "next/image";
+
+import { CardContent } from "@/components/ui/card";
+import { fadeIntoView } from "@/lib/motion";
+import MotionCard from "@/components/ui/motion/motion-card";
+
+interface TestimonialCardProps {
+  quote: string;
+  image: string | StaticImageData;
+  name: string;
+}
+
+export default function TestimonialCard({
+  quote,
+  image,
+  name,
+}: TestimonialCardProps) {
+  return (
+    <MotionCard {...fadeIntoView} className="bg-white">
+      <CardContent className="p-6">
+        <p className="mb-4 text-gray-600">&quot;{quote}&quot;</p>
+        <div className="flex 1s-center">
+          <Image
+            width={50}
+            height={50}
+            src={image}
+            alt="Customer 1"
+            className="mr-4 w-12 h-12 rounded-full"
+          />
+          <div>
+            <p className="font-semibold">{name}</p>
+            <p className="text-sm text-gray-250">Dog Parent</p>
+          </div>
+        </div>
+      </CardContent>
+    </MotionCard>
+  );
+}
