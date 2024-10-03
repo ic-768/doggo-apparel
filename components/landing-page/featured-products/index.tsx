89a7eab14726fc9f812e0bdb987c160012cd05a3
ic768 <ic768@protonmail.com>
2024-10-03 20:39:44 +0300

    remove reduntant file

--- components/landing-page/featured-products/index.tsx
+++ components/landing-page/featured-products/index.tsx
@@ -3,17 +3,9 @@
 import batdog from "@/images/outfits/batman.jpg";
 import bernard from "@/images/outfits/indian-bernard.jpg";
 import hat11 from "@/images/hats/hat-11.jpg";
-import { Card, CardContent } from "@/components/ui/card";
 import { motion } from "framer-motion";
-import Image from "next/image";
-import MotionButton from "../../ui/motion/motion-button";
-import { fadeIntoView, tapScale } from "@/lib/motion";
-
-const imageProps = {
-  className: "w-full rounded-lg",
-  width: 500,
-  height: 500,
-};
+import { fadeIntoView } from "@/lib/motion";
+import PreviewCard from "./preview-card";
 
 export default function FeaturedProducts() {
   return (
@@ -26,63 +18,24 @@ export default function FeaturedProducts() {
           Featured Products
         </motion.h2>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <motion.div {...fadeIntoView} whileHover={{ scale: 1.05 }}>
-            <Card>
-              <CardContent className="p-4">
-                <Image alt="cutely dressed dog" src={hat11} {...imageProps} />
-                <h3 className="mb-2 text-xl font-semibold">
-                  Fancy Dog Outfit 1
-                </h3>
-                <p className="mb-4 text-gray-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <MotionButton
-                  {...tapScale}
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700"
-                >
-                  View
-                </MotionButton>
-              </CardContent>
-            </Card>
-          </motion.div>
-          <motion.div {...fadeIntoView} whileHover={{ scale: 1.05 }}>
-            <Card>
-              <CardContent className="p-4">
-                <Image alt="cutely dressed dog" src={batdog} {...imageProps} />
-                <h3 className="mb-2 text-xl font-semibold">
-                  Fancy Dog Outfit 2
-                </h3>
-                <p className="mb-4 text-gray-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <MotionButton
-                  {...tapScale}
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700"
-                >
-                  View
-                </MotionButton>
-              </CardContent>
-            </Card>
-          </motion.div>
-          <motion.div {...fadeIntoView} whileHover={{ scale: 1.05 }}>
-            <Card>
-              <CardContent className="p-4">
-                <Image alt="cutely dressed dog" src={bernard} {...imageProps} />
-                <h3 className="mb-2 text-xl font-semibold">
-                  Fancy Dog Outfit 3
-                </h3>
-                <p className="mb-4 text-gray-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <MotionButton
-                  {...tapScale}
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700"
-                >
-                  View
-                </MotionButton>
-              </CardContent>
-            </Card>
-          </motion.div>
+          <PreviewCard
+            alt="awesome dog in an awesome hat"
+            src={hat11}
+            title="Doggo hat"
+            description="Made of 100% cotton, this cap keeps your dog cool on hot days, while the adjustable strap ensures a snug fit for dogs of all sizes."
+          />
+          <PreviewCard
+            alt="nanananananana Bat dog!"
+            src={batdog}
+            title="Batman costume"
+            description="This sleek, officially licensed outfit transforms your pup into Gotham’s Dark Knight for that signature vigilante look."
+          />
+          <PreviewCard
+            alt="saint bernard in beautiful indian attire"
+            src={bernard}
+            title="Indian outfit"
+            description="Dress your pup in timeless elegance. Whether it's a wedding, festival, or special occasion, your dog will turn heads in this regal ensemble."
+          />
         </div>
       </div>
     </section>
