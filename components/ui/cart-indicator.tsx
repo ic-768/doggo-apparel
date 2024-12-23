bff3d488f1eb5892f4f600ce9594e383824e2430
ic-768 <ic768@protonmail.com>
2024-12-23 20:42:55 +0200

    Cart indicator animation

--- components/ui/cart-indicator.tsx
+++ components/ui/cart-indicator.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { ShoppingCart } from "lucide-react";
 
 import { useShoppingCart } from "@/context/use-shopping-cart";
@@ -11,11 +12,23 @@ export default function CartIndicator({ id }: { id?: number }) {
 
   return (
     <div className="relative">
-      {numToShow > 0 ? (
-        <span className="absolute -top-3 -right-3 flex items-center justify-center w-5 h-5 text-xs text-white bg-red-500 rounded-full">
-          {numToShow}
-        </span>
-      ) : null}
+      {numToShow > 0 && (
+        <motion.span
+          className="absolute -top-3 -right-3 flex items-center justify-center w-5 h-5 text-xs text-white bg-red-500 rounded-full"
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.2 }}
+        >
+          <motion.span
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            key={numToShow}
+          >
+            {numToShow}
+          </motion.span>
+        </motion.span>
+      )}
+
       <ShoppingCart size={24} />
     </div>
   );
