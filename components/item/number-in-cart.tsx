9f2b481a4b2655a5f901c50b587fcb71c757a729
ic-768 <ic768@protonmail.com>
2024-12-28 17:27:10 +0200

    Fix hydration mistmatch

--- components/item/number-in-cart.tsx
+++ components/item/number-in-cart.tsx
@@ -6,10 +6,10 @@ import { ClothingItem } from "@/lib/types";
 import { Badge } from "../ui/badge";
 
 export default function NumberInCart({ item }: { item: ClothingItem }) {
-  const { getNumInCart } = useShoppingCart();
+  const { cart, getNumInCart } = useShoppingCart();
   const numInCart = getNumInCart(item.id);
 
-  return !numInCart ? null : (
+  return !cart ? null : (
     <Badge
       variant="secondary"
       className="px-2 py-1 outline outline-1 outline-neutral-300"
